32ce66e1c4d2f4b1d00bbb2339ef80b0ef12b33f
Luna <62033805+Luna5ama@users.noreply.github.com>
2022-12-11 18:08:45 -0500

    Gradle project setup

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    kotlin("jvm") version "1.7.22"
+}
+
+group = "dev.fastmc"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.ow2.asm:asm-commons:9.4")
+    implementation("org.ow2.asm:asm-tree:9.4")
+}
+
+kotlin {
+    val jvmArgs = mutableSetOf<String>()
+    (rootProject.findProperty("kotlin.daemon.jvm.options") as? String)
+        ?.split("\\s+".toRegex())?.toCollection(jvmArgs)
+    System.getProperty("gradle.kotlin.daemon.jvm.options")
+        ?.split("\\s+".toRegex())?.toCollection(jvmArgs)
+    kotlinDaemonJvmArgs = jvmArgs.toList()
+}
+
+tasks {
+    compileJava {
+        options.encoding = "UTF-8"
+        sourceCompatibility = "17"
+        targetCompatibility = "17"
+    }
+
+    compileKotlin {
+        kotlinOptions {
+            jvmTarget = "17"
+        }
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "ow2-asm-ktdsl"
